bc4724991657d2f5951f131c212a6586aad720cc
Nilavathani <98324692+NilavathaniAP@users.noreply.github.com>
2024-09-01 01:31:52 +0530

    Update Retailer_Management.sql

--- Supply Chain Management/Retailer_Management.sql
+++ Supply Chain Management/Retailer_Management.sql
@@ -121,23 +121,12 @@ set day_s6 = day_s5-Day_6
 drop table if exists #xt
 
 create table #xt
-(
-Orderid numeric,
-p_0 float,
-p_1 float,
-p_2 float,
-p_3 float,
-p_4 float,
-p_5 float,
-p_6 float,
-x_0 float,
-x_1 float,
-x_2 float,
-x_3 float,
-x_4 float,
-x_5 float,
-x_6 float);
+(Orderid numeric, p_0 float, p_1 float, p_2 float,
+p_3 float, p_4 float, p_5 float, p_6 float, 
+x_0 float, x_1 float, x_2 float, x_3 float,
+x_4 float, x_5 float, x_6 float);
 
+--Using Recursive CTE method for simplified calculation process
 with p_t as(
 select OrderID,
 0 as p_0 ,
@@ -157,7 +146,7 @@ select
 f.orderid,
 cast(f.Demand as float) as x_0,
 cast(f.Demand*pt.p_1 as float) as x_1
-, cast(null as float) as x_2 --placeholder for recursive usage
+, cast(null as float) as x_2                                     --use NULL as placeholder for recursive usage
 , cast(null as float) as x_3
 , cast(null as float) as x_4
 , cast(null as float) as x_5
@@ -178,29 +167,21 @@ fx.OrderID
 ,cast((fx.x_0*pt.p_4) +(fx.x_1*pt.p_3)+(fx.x_2*pt.p_2)+(fx.x_3*pt.p_1) as float)as x_4
 ,cast((fx.x_0*pt.p_5) +(fx.x_1*pt.p_4)+(fx.x_2*pt.p_3)+(fx.x_3*pt.p_2)+(fx.x_4*pt.p_1) as float) as x_5
 ,cast((fx.x_0*pt.p_6) +(fx.x_1*pt.p_5)+(fx.x_2*pt.p_4)+(fx.x_3*pt.p_3)+(fx.x_4*pt.p_2) +(fx.x_5*pt.p_1) as float)as x_6
-, fx.level+1 as level
+, fx.level+1 as level  
 from finding_xt fx
 join p_t pt
 on fx.OrderID=pt.OrderID
-where fx.level <6 --control from infinite recurrsion
+where fx.level <6                                                         --controls from infinite recurrsion
 )
+ 
 insert into #xt(Orderid,p_0,p_1,p_2,p_3,p_4,p_5,p_6,x_0,x_1,x_2,x_3,x_4,x_5,x_6)
 select 
 fx.OrderID,
-max(pt.p_0) as x_0
-,max(pt.p_1) as x_1,
-max(pt.p_2) as x_2 
-,max(pt.p_3) as x_3 
-,max(pt.p_4) as x_4
-,max(pt.p_5) as x_5 
-,max(pt.p_6) as x_6 ,
-max(fx.x_0) as x_0
-,max(fx.x_1) as x_1,
-max(fx.x_2) as x_2 
-,max(fx.x_3) as x_3 
-,max(fx.x_4) as x_4
-,max(fx.x_5) as x_5 
-,max(fx.x_6) as x_6 --use max to filter out repeatition
+max(pt.p_0) as x_0, max(pt.p_1) as x_1, max(pt.p_2) as x_2 
+,max(pt.p_3) as x_3, max(pt.p_4) as x_4, max(pt.p_5) as x_5 
+,max(pt.p_6) as x_6, max(fx.x_0) as x_0, max(fx.x_1) as x_1,
+max(fx.x_2) as x_2, max(fx.x_3) as x_3, max(fx.x_4) as x_4
+,max(fx.x_5) as x_5, max(fx.x_6) as x_6                                   --use max to filter out repeatition
 from finding_xt fx
 join p_t pt
 on fx.OrderID=pt.OrderID
